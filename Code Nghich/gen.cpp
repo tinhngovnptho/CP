b43dfe2a05c2d28f2b07ff272346028c33f7ae30
tinhngovnptho <tinhngo.ptho@gmail.com>
2024-12-20 14:37:49 +0700

    Add file 20-12

--- Code Nghich/gen.cpp
+++ Code Nghich/gen.cpp
@@ -1,41 +1,29 @@
+/*
+	Author: tinhnopro
+	created: 19.12.2024 17:21:14
+*/
 #include <bits/stdc++.h>
 
+#ifdef LOCAL
+#include "D:\Tinhnopro\header\debug.h"
+#else
+#define debug(...) 42
+#endif // LOCAL
+
 #define TASK ""
-#define TIME ((1.0 * clock()) / CLOCKS_PER_SEC)
 
 using namespace std;
 
 using i64 = long long;
 
-mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
-
-i64 randInt(i64 l, i64 r) {
-	return uniform_int_distribution<i64> (l, r) (rng);
-}
-
-char randaz() {
-	return char('a' + randInt(0, 25));
-}
-
-char randAZ() {
-	return char('A' + randInt(0, 25));
-}
-
-void gentest() {
-	ofstream cout(TASK".inp");
-
-	cout.close();
-}
 
 signed main() {
-	for (int i = 1; i <= 100; ++i) {
-		gentest();
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
-		system(TASK".exe");
-		system(TASK"_bf.exe");
-
-		if (system("fc " TASK ".out " TASK ".ans") != 0) {
-			return 0;
-		}
+	if (fopen(TASK".inp", "r")) {
+		freopen(TASK".inp", "r", stdin);
+		freopen(TASK".out", "w", stdout);
 	}
+	
 }
--- Code THPT/VOI Traning/24/impeval.cpp
+++ Code THPT/VOI Traning/24/impeval.cpp
@@ -1,73 +1,49 @@
-/*
-	Author: Tinhnopro (Tinh nop)
-	From: CHV Phu Tho
- */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
-namespace std {
-	// optimize min max
-	template<typename T1, typename T2> bool maximize(T1 &a, T2 b) { return a < b ? a = b, 1 : 0; }
-	template<typename T1, typename T2> bool minimize(T1 &a, T2 b) { return a > b ? a = b, 1 : 0; }
-	// vector
-	template<typename T, int D> struct vec : public vector<vec<T, D - 1>> {
-		static_assert(D >= 1, "Init error");
-		template<typename... Args>
-			vec(int n = 0, Args... args) : vector<vec<T, D - 1>>(n, vec<T, D - 1>(args...)) {}
-	};
-	template<typename T> struct vec<T, 1> : public vector<T> {
-		vec(int n = 0, T val = T()) : vector<T>(n, val) {}
-		template<typename InputIt>
-			vec(InputIt first, InputIt last) : vector<T>(first, last) {}
-	};
-	// size
-	template<typename T> int Size(const T &a) {  return a.size(); }
-}
+#define TASK "impeval"
+
 using namespace std;
 
-const int MAXN = 1e5 + 11;
+using i64 = long long;
 
-struct Edge {
-    int u, v, w;
-    Edge(int _u, int _v, int _w)  : u(_u), v(_v), w(_w) {}
-};
+const int maxN = 1e5 + 5; 
 
 struct Node {
-    int v, w;
-    Node(int _v, int _w) : v(_v), w(_w) {}
+	int v, w; 
+
+	Node (int _v = 0, int _w = 0) : v(_v), w(_w) {}
 };
 
-int n, m, p;
-vector<Node> adj[MAXN];
-vector<Edge> edges;
+struct edge_t {
+	int u, v, w; 
 
-namespace Sub1 {
-	void solve(void) {
-		for (int i = 1)
-	}
+	edge_t (int _u = 0, int _v = 0, int _w = 0) : u(_u), v(_v), w(_w) {}
 };
 
-void process(void) {
-    cin >> n >> m >> p;
-    for (int i = 1, u, v, w; i <= m; ++i) {
-        cin >> u >> v >> w;
-        edges.emplace_back(u, v, w);
-        adj[u].emplace_back(v, w);
-        adj[v].emplace_back(u, w);
-    }
-    if (m <= 1000 && p <= 1000) return void(Sub1::solve());
-}
+int n, m, q; 
+vector<Node> adj[maxN]; 
+edge_t edges[maxN], queries[maxN]; 	
 
-int32_t main(void) {
-	ios_base::sync_with_stdio(false); cin.tie(nullptr);
-	#define name "impeval"
-	if (fopen(name".inp", "r")) {
-		freopen(name".inp", "r", stdin);
-		freopen(name".out", "w", stdout);
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	if (fopen(TASK".inp", "r")) {
+		freopen(TASK".inp", "r", stdin);
+		freopen(TASK".out", "w", stdout);
 	}
 
-	// int t; cin >> t; while (t--)
-	process();
+	cin >> n >> m >> q; 
+	for (int i = 1; i <= m; ++i) {
+		int u, v, w; 
+		cin >> u >> v >> w; 
+		edges[i] = edge_t(u, v, w); 
+		adj[u].emplace_back(v, w); 
+		adj[v].emplace_back(u, w);
+	}
 
-	cerr << "\nRuntime: " << (1.0 * clock() / CLOCKS_PER_SEC) << "s. Code by Tinhnopro\n";
-	return 0;
+	for (int i = 1; i <= q; ++i) 
+		cin >> queries[i].u >> queries[i].v >> queries[i].w; 
+	
+	
 }
